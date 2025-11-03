03197cbafecf9189f83ea0e47c6fe63d962c9589
Dishant Goyal <truefacts9877@gmail.com>
2025-11-03 19:59:23 +0530

    Add start script and prepare backend for render

--- Backend/src/utils/cloudinary.js
+++ Backend/src/utils/cloudinary.js
@@ -1,7 +1,7 @@
 import { v2 as cloudinary } from "cloudinary";
 
 import fs from "fs"
-import { ApiError } from "./ApiError.js";
+import { ApiError } from "./apiError.js";
 
 
 const uploadOnCloudinary=async(filePath,folder)=>{

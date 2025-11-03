10c9d4a100260446046a20d660830d7c7a5443af
Dishant Goyal <truefacts9877@gmail.com>
2025-11-03 19:57:02 +0530

    Add start script and prepare backend for render

--- Backend/src/controller/post.controller.js
+++ Backend/src/controller/post.controller.js
@@ -1,5 +1,5 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/ApiError.js";
+import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import Post from "../models/post.model.js";
